5793088f832959898a702f99df1ef99a1ec0da44
erinscheunemann <89548761+erinscheunemann@users.noreply.github.com>
2024-02-18 16:36:20 -0800

    fishing boat and net encounter changes

--- javascript/boat.js
+++ javascript/boat.js
@@ -1,19 +1,13 @@
 window.addEventListener('load', function() {
     // get swim button
     let btn = document.getElementById('cs');
-    // get days
-    let days = Number(sessionStorage.getItem('days'));
 
     btn.addEventListener('click', function() {
-        sessionStorage.setItem('days', days+2); // add two days to total days traveled 
         let diceRoll = Math.random(); // roll for survival
         if (diceRoll <= 0.1) {
             window.location.replace('net.html');
-        } else if (diceRoll > 0.1 && diceRoll <=0.2) { // get hit and killed
-            sessionStorage.setItem('outcome', 'b'); // set outcome to boat death
-            window.location.replace('result.html');
         } else {
-            window.location.replace('swim.html');
+            window.location.replace('avoidnet.html');
         }
     });
 });
